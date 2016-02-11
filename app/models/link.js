e1d8c524be82cd4e18640bd93710ea74fde10033
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 15:52:22 -0800

    cleaned up code comments

--- app/models/link.js
+++ app/models/link.js
@@ -20,24 +20,8 @@ var linkSchema = new db.Schema( {
     type: Number,
     default: 0
   }
-
 } );
 
 var Link = db.model( 'Url', linkSchema );
-
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function() {
-//     this.on('creating', function(model, attrs, options) {
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
 
 module.exports = Link;
--- app/models/user.js
+++ app/models/user.js
@@ -23,35 +23,13 @@ User.getHashedPassword = function( userID, callback ) {
 };
 
 User.comparePassword = function( userID, attemptedPassword, callback ) {
-  console.log("Within comparepassword: userID", userID);
-  console.log("Within comparepassword: attpass", attemptedPassword);
+
   User.getHashedPassword( userID, function( password ) {
-    console.log("withingetHashedPass call, password is:", password);
     bcrypt.compare(attemptedPassword, password, function(err, isMatch) {
       callback(isMatch);
     } );
   } );
 };
-
-// User.hashPassword = function( userID, callback ) {
-//   User.findById( userID, function( err, user ) {
-//     if( err ) {
-//       throw err;
-//     }
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher( user.password, null, null )
-//       .then( function( hash ) {
-//         user.password = hash;
-//         user.save( function( err ) {
-//           if( err ) {
-//             throw err;
-//           } else {
-//             callback();
-//           }
-//         } );
-//       });
-//   } );
-// };
 
 userSchema.pre( 'save', function( next ) {
   var cipher = Promise.promisify( bcrypt.hash );
